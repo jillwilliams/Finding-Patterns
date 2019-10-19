d43b3a0ca75969b874dea0edc3a40785321d0c08
jillwilliams <codeGeek.03.14@gmail.com>
2019-10-19 11:44:11 -0700

    patterns

---
+++ script.js
@@ -0,0 +1,23 @@
+// On buttyon click reveal a blue heptagon-pattern 1
+const blueHep = () => {
+    return "Blue Heptagon";
+};
+const pattern1 = ( ) => {
+    document.getElementById("demo").innerHTML = blueHep();
+}
+
+// On buttyon click reveal pattern 2
+const Saturday = () => {
+    return "S, for Saturday";
+};
+const pattern2 = ( ) => {
+    document.getElementById("demo1").innerHTML = Saturday();
+}
+
+// On buttyon click reveal pattern 3
+const ten = () => {
+    return "10";
+};
+const pattern3 = ( ) => {
+    document.getElementById("demo2").innerHTML = ten();
+}
